48e151457af736e8ad7627378628caf12410298c
om.makana@gmail.com <om.makana@gmail.com>
2020-11-16 11:26:20 +0530

    adding sign in redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./components/header/header";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route , useHistory} from "react-router-dom";
 import "./App.css";
 import YoutubeComponent from "./components/youtube/youtube";
 import LoginComponent from "./components/login/login";
 import AddYoutubeLink from "./components/addYoutubeLink/addYoutubeLink";
+import { Security, SecureRoute, LoginCallback } from "@okta/okta-react";
 
 function App() {
+
+  const history = useHistory();
+
+  function onAuthRequired() {
+    history.push("/login");
+  }
   return (
     <div className="app">
       <Router>
@@ -15,19 +22,31 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/youtube">
-            <YoutubeComponent />
-          </Route>
-          <Route path="/camera">
-            <div>camera</div>
-          </Route>
-          <Route path="/login">
-            {/* <LoginComponent /> */}
-            <AddYoutubeLink />
-          </Route>
-          <Route path="/">
+          <Route path="/" exact={true}>
             <div>home</div>
           </Route>
+          <Route path="/youtube" exact={true}>
+            <YoutubeComponent />
+          </Route>
+          <Security
+            issuer="https://dev-2806996.okta.com/oauth2/default"
+            clientId="0oaoe889ciKp2qjIW5d5"
+            redirectUri={window.location.origin + "/login/callback"}
+            onAuthRequired={onAuthRequired}
+            pkce={true}
+          >
+            <SecureRoute path="/admin" exact={true}>
+              <AddYoutubeLink />
+            </SecureRoute>
+            <Route
+              path="/login"
+              exact={true}
+              render={() => (
+                <LoginComponent baseUrl="https://dev-2806996.okta.com" />
+              )}
+            />
+            <Route path="/login/callback" component={LoginCallback} />
+          </Security>
         </Switch>
       </Router>
     </div>
--- src/components/addYoutubeLink/addYoutubeLink.js
+++ src/components/addYoutubeLink/addYoutubeLink.js
@@ -4,120 +4,130 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import "./addYoutubeLink.scss";
 import Loader from "../loader/loader";
+import { withOktaAuth } from '@okta/okta-react';
 
-function AddYoutubeLink() {
-  const nameRef = React.createRef();
-  const imageRef = React.createRef();
-  const youtubeRef = React.createRef();
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (
-      nameRef.current.value === "" ||
-      imageRef.current.value === "" ||
-      youtubeRef.current.value === ""
-    ) {
-      setErrorMessage("enter all details");
-      return;
+export default withOktaAuth (
+  function AddYoutubeLink(props) {
+    const nameRef = React.createRef();
+    const imageRef = React.createRef();
+    const youtubeRef = React.createRef();
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      if (
+        nameRef.current.value === "" ||
+        imageRef.current.value === "" ||
+        youtubeRef.current.value === ""
+      ) {
+        setErrorMessage("enter all details");
+        return;
+      }
+      setLoading(true);
+      axios
+        .post("/youtube", {
+          name: nameRef.current.value,
+          imageUrl: imageRef.current.value,
+          youtube: youtubeRef.current.value,
+        })
+        .then((resp) => {
+          setLoading(false);
+          console.log(resp);
+          setErrorMessage(null);
+          setSuccessMessage("Data added successfully..");
+        })
+        .catch((err) => {
+          setLoading(false);
+          setSuccessMessage(null);
+          setErrorMessage("Data could not be added successfully..");
+        });
     }
-    setLoading(true);
-    axios
-      .post("/youtube", {
-        name: nameRef.current.value,
-        imageUrl: imageRef.current.value,
-        youtube: youtubeRef.current.value,
-      })
-      .then((resp) => {
-        setLoading(false);
-        console.log(resp);
-        setErrorMessage(null);
-        setSuccessMessage("Data added successfully..");
-      })
-      .catch((err) => {
-        setLoading(false);
-        setSuccessMessage(null);
-        setErrorMessage("Data could not be added successfully..");
-      });
-  }
-
-  function handleDelete(event) {
-    event.preventDefault();
-    if (nameRef.current.value === "") {
-      setErrorMessage("enter name");
-      return;
+  
+    function handleDelete(event) {
+      event.preventDefault();
+      if (nameRef.current.value === "") {
+        setErrorMessage("enter name");
+        return;
+      }
+      setLoading(true);
+      axios
+        .delete(`/youtube/${nameRef.current.value}`, {
+          name: nameRef.current.value,
+        })
+        .then((resp) => {
+          setLoading(false);
+          setSuccessMessage("Data removed successfully..");
+          setErrorMessage(null);
+        })
+        .catch((err) => {
+          setLoading(false);
+          setErrorMessage(null);
+          setSuccessMessage(null);
+          setErrorMessage("Data could not be removed...");
+        });
     }
-    setLoading(true);
-    axios
-      .delete(`/youtube/${nameRef.current.value}`, {
-        name: nameRef.current.value,
-      })
-      .then((resp) => {
-        setLoading(false);
-        setSuccessMessage("Data removed successfully..");
-        setErrorMessage(null);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setErrorMessage(null);
-        setSuccessMessage(null);
-        setErrorMessage("Data could not be removed...");
-      });
+    return (
+      <>
+        {!loading ? (
+          <form autoComplete="off" className="addYoutubeLinkForm">
+            {successMessage && (
+              <div className="addYoutubeLinkForm__message success">{successMessage}</div>
+            )}
+            <TextField
+              inputRef={nameRef}
+              id="outlined-basic-1"
+              label="name"
+              variant="outlined"
+            />
+            <TextField
+              inputRef={imageRef}
+              id="outlined-basic-2"
+              label="image-url"
+              variant="outlined"
+            />
+            <TextField
+              inputRef={youtubeRef}
+              id="outlined-basic-3"
+              label="youtube-url"
+              variant="outlined"
+            />
+            <Button
+              onClick={handleSubmit}
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              Add data
+            </Button>
+  
+            <Button
+              onClick={handleDelete}
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              Delete Data
+            </Button>
+  
+            <Button
+              onClick={()=>props.authService.logout()}
+              variant="contained"
+              color="secondary"
+              type="submit"
+            >
+             Logout
+            </Button>
+
+            {errorMessage && (
+              <div className="addYoutubeLinkForm__message error">{errorMessage}</div>
+            )}
+          </form>
+        ) : (
+          <Loader />
+        )}
+      </>
+    );
   }
-  return (
-    <>
-      {!loading ? (
-        <form autoComplete="off" className="addYoutubeLinkForm">
-          {successMessage && (
-            <div className="addYoutubeLinkForm__message success">{successMessage}</div>
-          )}
-          <TextField
-            inputRef={nameRef}
-            id="outlined-basic-1"
-            label="name"
-            variant="outlined"
-          />
-          <TextField
-            inputRef={imageRef}
-            id="outlined-basic-2"
-            label="image-url"
-            variant="outlined"
-          />
-          <TextField
-            inputRef={youtubeRef}
-            id="outlined-basic-3"
-            label="youtube-url"
-            variant="outlined"
-          />
-          <Button
-            onClick={handleSubmit}
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
-            Add data
-          </Button>
-
-          <Button
-            onClick={handleDelete}
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
-            Delete Data
-          </Button>
-
-          {errorMessage && (
-            <div className="addYoutubeLinkForm__message error">{errorMessage}</div>
-          )}
-        </form>
-      ) : (
-        <Loader />
-      )}
-    </>
-  );
-}
-
-export default AddYoutubeLink;
+)
